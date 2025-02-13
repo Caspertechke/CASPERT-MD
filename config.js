2b95ea27e368da7ff67fdc6a5fa2c21b48cccdef
Casper tech <146692830+Traby-qriz@users.noreply.github.com>
2025-02-13 06:19:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,|| ""
+SESSION_ID: process.env.SESSION_ID,|| "CASPER-TECH~Gn4kjKqT#sB7xX3WA78prkJ7HIBbufglTqyttIv9xvtEZ1TccFn8"
     
     ALIVE_IMG: process.env.ALIVE_IMG,|| " "
     ALIVE_MSG: process.env.ALIVE_MSG,|| "🄲🄰🅂🄿🄴🅁-🅇🄼🄳 is alive😜 dude 😎 "
