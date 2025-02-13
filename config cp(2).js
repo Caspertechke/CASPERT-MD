bbc01e614c307d3c11543d3f708787102af9f3e0
Casper tech <146692830+Traby-qriz@users.noreply.github.com>
2025-02-13 05:57:20 +0300

    Add files via upload

---
+++ config cp(2).js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID,|| ""
+    
+    ALIVE_IMG: process.env.ALIVE_IMG,|| " "
+    ALIVE_MSG: process.env.ALIVE_MSG,|| "🄲🄰🅂🄿🄴🅁-🅇🄼🄳 is alive😜 dude 😎 "
+};
